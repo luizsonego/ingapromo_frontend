8d785937f3b30a127624162d507370f158d78367
Luiz Yavorski <leesonego@gmail.com>
2022-07-13 22:59:58 -0300

    usser navbar

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom'
 import Logo from '../Logo'
 
 var isLogged = localStorage.getItem(process.env.REACT_APP_ACCESS_TOKEN)
+const userInfo = {
+  name: 'Guest',
+  profilePic:
+    'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+}
 const menuOptions = [
   {
     name: 'Inicio',
@@ -12,10 +17,6 @@ const menuOptions = [
   //   name: 'Cupons',
   //   link: '/cupons',
   // },
-  {
-    name: isLogged ? 'Sair' : 'Entrar/Cadastrar',
-    link: isLogged ? '/logout' : '/login',
-  },
 ]
 
 const NavBar = () => {
@@ -31,33 +32,6 @@ const NavBar = () => {
                 {/* 
                 hidden on XL
                  */}
-                {/* <div className="visible xl:hidden">
-                  <ul className="p-2 border-r bg-white absolute rounded left-0 right-0 shadow mt-8 md:mt-8 hidden">
-                    <li className="flex xl:hidden cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-2 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
-                      <div className="flex items-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          className="icon icon-tabler icon-tabler-grid"
-                          width={20}
-                          height={20}
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          fill="none"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        >
-                          <path stroke="none" d="M0 0h24v24H0z" />
-                          <rect x={4} y={4} width={6} height={6} rx={1} />
-                          <rect x={14} y={4} width={6} height={6} rx={1} />
-                          <rect x={4} y={14} width={6} height={6} rx={1} />
-                          <rect x={14} y={14} width={6} height={6} rx={1} />
-                        </svg>
-                        <span className="ml-2 font-bold">Dashboard</span>
-                      </div>
-                    </li>
-                  </ul>
-                </div> */}
                 {/* 
                 CLOSE MENU
                  */}
@@ -121,72 +95,24 @@ const NavBar = () => {
                     >
                       {profile && (
                         <ul className="p-2 w-40 border-r bg-white absolute rounded right-0 shadow top-0 mt-16 ">
-                          <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
+                          <li className="my-2 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
+                            <Link to="/admin">
+                              <div className="flex items-center">
+                                <span className="ml-2">Painel</span>
+                              </div>
+                            </Link>
+                          </li>
+                          <li className="my-2 cursor-pointer text-gray-600 text-sm leading-3 tracking-normal py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none">
                             <div className="flex items-center">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="icon icon-tabler icon-tabler-user"
-                                width={20}
-                                height={20}
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                fill="none"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              >
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <circle cx={12} cy={7} r={4} />
-                                <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-                              </svg>
-                              <span className="ml-2">My Profile</span>
+                              <span className="ml-2">Sair</span>
                             </div>
-                          </li>
-                          <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-2 py-2 hover:text-indigo-700 focus:text-indigo-700 focus:outline-none flex items-center">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="icon icon-tabler icon-tabler-help"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              fill="none"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            >
-                              <path stroke="none" d="M0 0h24v24H0z" />
-                              <circle cx={12} cy={12} r={9} />
-                              <line x1={12} y1={17} x2={12} y2="17.01" />
-                              <path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" />
-                            </svg>
-                            <span className="ml-2">Help Center</span>
-                          </li>
-                          <li className="cursor-pointer text-gray-600 text-sm leading-3 tracking-normal mt-2 py-2 hover:text-indigo-700 flex items-center focus:text-indigo-700 focus:outline-none">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className="icon icon-tabler icon-tabler-settings"
-                              width={20}
-                              height={20}
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              fill="none"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                            >
-                              <path stroke="none" d="M0 0h24v24H0z" />
-                              <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                              <circle cx={12} cy={12} r={3} />
-                            </svg>
-                            <span className="ml-2">Account Settings</span>
                           </li>
                         </ul>
                       )}
                       <div className="cursor-pointer flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-white transition duration-150 ease-in-out">
                         <img
                           className="rounded-full h-10 w-10 object-cover"
-                          src="https://tuk-cdn.s3.amazonaws.com/assets/components/horizontal_navigation/hn_2.png"
+                          src={userInfo.profilePic}
                           alt="logo"
                         />
                       </div>
@@ -320,6 +246,18 @@ const NavBar = () => {
                           </li>
                         </Link>
                       ))}
+                    <hr className="mt-5" />
+                    {isLogged && (
+                      <Link to="/admin" className="cursor-pointer">
+                        <li className="text-gray-800 pt-5">
+                          <div className="flex items-center">
+                            <p className="text-indigo-700 xl:text-base text-base ml-3">
+                              Painel
+                            </p>
+                          </div>
+                        </li>
+                      </Link>
+                    )}
                   </ul>
                 </div>
                 {/* 
@@ -332,49 +270,16 @@ const NavBar = () => {
                         <div className="flex items-center">
                           <img
                             alt="profile-pic"
-                            src="https://tuk-cdn.s3.amazonaws.com/assets/components/boxed_layout/bl_1.png"
+                            src={userInfo.profilePic}
                             className="w-8 h-8 rounded-md"
                           />
                           <p className=" text-gray-800 text-base leading-4 ml-2">
-                            Jane Doe
+                            {userInfo.name}
                           </p>
                         </div>
                         <ul className="flex">
-                          <li className="cursor-pointer text-gray-800 pt-5 pb-3">
-                            <div className="w-6 h-6 md:w-8 md:h-8">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="icon icon-tabler icon-tabler-messages"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1}
-                                stroke="currentColor"
-                                fill="none"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              >
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
-                                <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
-                              </svg>
-                            </div>
-                          </li>
                           <li className="cursor-pointer text-gray-800 pt-5 pb-3 pl-3">
-                            <div className="w-6 h-6 md:w-8 md:h-8">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="icon icon-tabler icon-tabler-bell"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1}
-                                stroke="currentColor"
-                                fill="none"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              >
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
-                                <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
-                              </svg>
-                            </div>
+                            <div className="w-6 h-6 md:w-8 md:h-8">sair</div>
                           </li>
                         </ul>
                       </div>
