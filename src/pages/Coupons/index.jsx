6e4d94eb691702755e56e27cede162bedd99f082
luiz yavorski <leesonego@gmail.com>
2022-10-12 23:47:03 -0300

    styled pagination

--- src/pages/Coupons/index.jsx
+++ src/pages/Coupons/index.jsx
@@ -25,6 +25,7 @@ const AllCoupons = () => {
     }
   );
 
+
   return (
     <section className="container mx-auto pt-12">
       <div className="mx-auto flex flex-col lg:flex-row gap-2">
@@ -36,26 +37,32 @@ const AllCoupons = () => {
           {data?.map((item) => (
             <Card data={item} key={item.id} />
           ))}
-        </div>
 
-        <div>
-          <button
-            onClick={() => setPage((old) => Math.max(old - 1, 1))}
-            disabled={page === 1}
-          >
-            Página Anterior
-          </button>
-          <span>Página Atual: {page}</span>
-          <button
-            onClick={() =>
-              setPage((old) => (linkPagination[1].pageCount ? old + 1 : old))
-            }
-            // disabled={
-            //   linkPagination[0].last.href
-            // }
-          >
-            Próxima Página
-          </button>
+          <div className="flex justify-center items-center gap-3 mt-12">
+            <button
+              className="bg-primary px-7 py-3 rounded"
+              onClick={() => {
+                window.scrollTo({ top: 0 });
+                setPage((old) => Math.max(old - 1, 1))
+              }}
+              disabled={page === 1}
+            >
+              {'< Página Anterior'}
+            </button>
+            <span>Página Atual: {page}</span>
+            <button
+              className="bg-primary px-7 py-3 rounded"
+              onClick={() =>{
+                window.scrollTo({ top: 0 });
+                setPage((old) => (linkPagination[1].pageCount ? old + 1 : old))
+              }}
+              // disabled={
+              //   linkPagination[0].last.href
+              // }
+            >
+              {'Próxima Página >'}
+            </button>
+          </div>
         </div>
       </div>
     </section>
